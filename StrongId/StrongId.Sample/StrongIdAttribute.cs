7168e07357f284a76942406401a66792f9333b83
Anton Christiansen <AntonChristiansen@proton.me>
2025-04-05 05:41:01 +0200

    Added parameters to the StrongId Attribute. ParameterType (The Type of the underlying Id, defaults to System.Guid) ParameterName (The property name of the strongly typed identifier)

--- StrongId/StrongId.Sample/StrongIdAttribute.cs
+++ StrongId/StrongId.Sample/StrongIdAttribute.cs
@@ -2,8 +2,9 @@ using System;
 
 namespace StrongId.Sample;
 
-[System.AttributeUsage(System.AttributeTargets.Class)]
+[AttributeUsage(AttributeTargets.Class)]
 public class StrongIdAttribute : Attribute
 {
-    
+    public Type ParameterType { get; set; } = typeof(Guid);
+    public string ParameterName { get; set; } = "Id";
 }
